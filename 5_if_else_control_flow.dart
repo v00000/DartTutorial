eb009ef954f37c766dd0f9f40e7f936cd7fb3bf9
v00000 <50797266+v00000@users.noreply.github.com>
2019-05-22 11:20:38 +0530

    try
    Yes

--- 5_if_else_control_flow.dart
+++ 5_if_else_control_flow.dart
@@ -4,7 +4,7 @@ void main() {
 	// IF and ELSE Statements
 	var salary = 15000;
 
-	if (salary > 20000) {
+	if (salary > 200) {
 		print("You got promotion. Congratulations !");
 	} else {
 		print("You need to work hard !");
